0e86490ebbd243ccea29d210217ae345e9f76370
Jakob <haruhunab1320@gmail.com>
2024-09-08 18:44:02 -0700

    add gxp storage and delete image after upload

--- src/services/Job-Queue/worker.ts
+++ src/services/Job-Queue/worker.ts
@@ -3,8 +3,18 @@ import { Worker, Job } from "bullmq";
 import { redisConfig } from "../redis/redisConfig";
 import { PrismaClient } from "@prisma/client";
 import { describeImage } from "../openai";
+import { Storage } from "@google-cloud/storage";
+import * as fs from "fs";
 
 const prisma = new PrismaClient();
+
+const storage = new Storage({
+  projectId: process.env["GOOGLE_CLOUD_PROJECT_ID"],
+  credentials: {
+    client_email: process.env["GOOGLE_CLOUD_CLIENT_EMAIL"],
+    private_key: process.env["GOOGLE_CLOUD_PRIVATE_KEY"]?.replace(/\\n/g, "\n"),
+  },
+});
 // Define the type for job data
 interface JobData {
   imagePath: string;
@@ -18,14 +28,35 @@ export const imageWorker = new Worker<JobData>(
 
     console.log(`Processing image at path: ${imagePath}`);
 
+    const [metadata] = await storage
+      .bucket(process.env["GOOGLE_CLOUD_STORAGE_BUCKET"] as string)
+      .file(imagePath)
+      .getSignedUrl({
+        version: "v4",
+        action: "read",
+        expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+      });
+
+    if (!metadata) {
+      console.error("Failed to get signed URL for image");
+      return;
+    }
+
     // 1. Send the image to OpenAI for description
     const description = await describeImage(imagePath);
+    if (!description) {
+      console.error("Failed to describe image");
+      return;
+    }
+
     await prisma.image.create({
       data: {
-        filePath: imagePath,
+        url: metadata[0],
         description,
       },
     });
+
+    await fs.promises.unlink(imagePath);
 
     console.log(
       `Image metadata saved to database with description: ${description}`
