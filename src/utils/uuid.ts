85d7c5880afc1cffbd5fc176075e3d890798574d
Jakob <haruhunab1320@gmail.com>
2024-09-09 21:36:20 -0700

    add new uuids

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -2,6 +2,13 @@ export const SERVICE_UUID = "19B10000-E8F2-537E-4F6C-D104768A1214"; // Main Frie
 
 export const AUDIO_DATA_UUID = "19B10001-E8F2-537E-4F6C-D104768A1214"; // Audio Data Characteristic
 export const AUDIO_CODEC_UUID = "19B10002-E8F2-537E-4F6C-D104768A1214"; // Audio Codec Characteristic
-export const PHOTO_DATA_UUID = "19B10005-E8F2-537E-4F6C-D104768A1214"; // Photo Data Characteristic
+export const AUDIO_CONTROL_UUID = "19B10003-E8F2-537E-4F6C-D104768A1214"; // Audio Control Characteristic
+export const AUDIO_ACK_UUID = "19B10004-E8F2-537E-4F6C-D104768A1214"; // Audio Ack Characteristic
+
 export const PHOTO_CONTROL_UUID = "19B10006-E8F2-537E-4F6C-D104768A1214"; // Photo Control Characteristic
+export const PHOTO_DATA_UUID = "19B10005-E8F2-537E-4F6C-D104768A1214"; // Photo Data Characteristic
 export const PHOTO_ACK_UUID = "19B10007-E8F2-537E-4F6C-D104768A1214"; // Photo Ack Characteristic
+
+export const BATTERY_LEVEL_UUID = "19B10008-E8F2-537E-4F6C-D104768A1214"; // Battery Level Characteristic
+
+export const DEVICE_CONTROL_UUID = "19B10009-E8F2-537E-4F6C-D104768A1214"; // Device Control Characteristic
