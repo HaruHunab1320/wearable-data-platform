4b9ce521e4da3bcfc80a0f57e2767793c739e92d
Jakob <haruhunab1320@gmail.com>
2024-09-11 15:16:28 -0700

    remove unused routes

--- src/pages/api/image.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Handle image data reception
-  // Process and save the image data
-  res.status(200).json({ message: "Image data received" });
-}
